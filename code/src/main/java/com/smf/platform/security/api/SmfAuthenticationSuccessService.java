6b4ac8bc92d4430aec1b07c5e72f5ff48e4fde2f
tianfei1943 <041621219@163.com>
2014-11-04 17:40:19 +0800

    b

--- code/src/main/java/com/smf/platform/security/api/SmfAuthenticationSuccessService.java
+++
@@ -1,8 +0,0 @@
-package com.smf.platform.security.api;
-
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-public interface SmfAuthenticationSuccessService extends
-		AuthenticationSuccessHandler {
-
-}
